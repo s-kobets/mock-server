0fbd7fac904f6c53124e07254acbbc4d3a14973d
s-kobets <s-kobets617@yandex.ru>
2017-08-01 23:30:05 +0300

    init project and post request register

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(express.static(__dirname + "/"));
+app.set('view engine', 'html');
+
+app.get('*', function(req, res) {
+    res.render('index.html')
+});
+
+app.post('/register', urlencodedParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+        console.log(req.body)
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(req.body));
+    // response.send(`${request.body.userName} - ${request.body.userAge}`)
+})
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
