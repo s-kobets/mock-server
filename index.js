749e833dc657869eb6191638337a8e990bed6d53
skobets <sergey.kobets@kupibilet.ru>
2017-08-02 13:57:17 +0300

    creact method getData for forms

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const MyForm = {
         errorFields: 'String',
     }),
 
-    getData: () => 'Object',
-
     setData: (Object) => undefined,
 
     submit: () => undefined,
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const elements = document.forms["myForm"].elements
+    MyForm.getData = getData
+
+    function getData(elements) {
+        const elementForm = {}
+        for (i = 0; i < elements.length; i += 1) {
+            if (elements[i].name) {
+                elementForm[elements[i].name] = elements[i].value
+            }
+        }
+        return elementForm
+    }
+
+    MyForm.getData(elements)
 
 }, false)
